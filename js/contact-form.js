4fb0a3ce8ae9e9db64832e4285f1a3e3e221868a
Hoa <hoa.nguyen@abaxtas.de>
2017-08-07 00:08:24 +0700

    update mail sender

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,52 +1,58 @@
 var contactForm = (function () {
     // --------------------------------- Public Declaration -------------------------------------------
     var init;
-    var sendMail;
+
 
     // --------------------------------- Private Variables ------------------------------------------- 
+    var _FORM_URL = 'http://formspree.io/nxhoaf@gmail.com';
     var _validator;
     // --------------------------------- Private Functions -------------------------------------------
-
+    var _sendMail;
 
     // --------------------------------- Public function -------------------------------------------    
     init = function () {
-        _validator = $('#sendMailForm').validator()
+        _validator = $('#sendMailForm').validator();
         _validator.on('submit', function (e) {
-            if (e.isDefaultPrevented()) {
-            } else {
-                sendMail();
+            if (!e.isDefaultPrevented()) {
+                _sendMail();
             }
             return false;
-        })
-        // $('#sendMail').click(sendMail);
-
-        // formUtils.submit({
-        //     action: 'http://formspree.io/nxhoaf@gmail.com',
-        //     method: 'post',
-        //     params: {
-        //         email: 'sender@gmail',
-        //         message: 'This is an email content'
-        //     }
-        // });
+        });
     };
 
-    sendMail = function (event) {
+
+    // --------------------------------- End of Public function -------------------------------------------
+
+
+    // --------------------------------- Private function -----------------------------------------------
+    _sendMail = function (event) {
         var name = $('#name').val();
         var email = $('#email').val();
         var subject = $('#subject').val();
         var message = $('#message').val();
 
-        var mailContent = " name: " + name + 
-            "\n subject: " + subject + 
+        var mailContent = " name: " + name +
+            "\n subject: " + subject +
             "\n message: " + message;
+
+        $.ajax({
+            url: _FORM_URL,
+            type: 'POST',
+            data: {
+                email: email,
+                message: mailContent
+            },
+            success: function (msg) {
+                $("#sendmessage").show().delay(3000).fadeOut();
+            },
+            error: function (request, status, error) {
+                $("#errormessage").show().delay(3000).fadeOut();
+            }
+        });
 
         return false;
 
     };
-    // --------------------------------- End of Public function -------------------------------------------
-
-
-    // --------------------------------- Private function -----------------------------------------------
 
     // --------------------------------- End of Private function -------------------------------------------
 
